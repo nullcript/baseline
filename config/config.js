3956e99e1581c15558734b9de4ae75d85cd45165
nullcript <nullcript@gmail.com>
2023-10-17 16:57:08 +0330

    add services and seedersStorage to project

--- config/config.js
+++ config/config.js
@@ -7,15 +7,22 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    database: "database_development",
+    database: "database-development",
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIAL,
+    // Use a different storage. Default: none
+    seederStorage: "json",
+    // Use a different file name. Default: sequelize-data.json
+    seederStoragePath: "./seeders/sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    seederStorageTableName: "sequelize_data",
     pool: {
       min: 0,
       max: 5,
       idle: 10000,
       acquire: 30000,
     },
+    logging:false
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -24,12 +31,19 @@ module.exports = {
     database: "database_test",
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIAL,
+    // Use a different storage. Default: none
+    seederStorage: "json",
+    // Use a different file name. Default: sequelize-data.json
+    seederStoragePath: "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    seederStorageTableName: "sequelize_data",
     pool: {
       min: 0,
       max: 5,
       idle: 10000,
       acquire: 30000,
     },
+    logging:false
   },
   production: {
     username: process.env.DB_USERNAME,
@@ -38,11 +52,18 @@ module.exports = {
     database: "database_production",
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIAL,
+    // Use a different storage. Default: none
+    seederStorage: "json",
+    // Use a different file name. Default: sequelize-data.json
+    seederStoragePath: "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    seederStorageTableName: "sequelize_data",
     pool: {
       min: 0,
       max: 5,
       idle: 10000,
       acquire: 30000,
     },
+    logging:false
   },
 };
